d843db7c0f8936bccc7e3b6464666be5144ce4fa
羽波 <umichangadaisuki@gmail.com>
2024-04-07 02:12:51 +0800

    新增IM-2 斗蛐蛐计算器 2

--- js/ququ2.js
+++ js/ququ2.js
@@ -1,5 +1,5 @@
 const NAME = 'IM-2 斗蛐蛐计算器';
-const VERSION = '1.01';
+const VERSION = '2.02';
 
 const CONTENTS = {
     'settings': {
@@ -29,11 +29,12 @@ let current_combo = 0;
 let jieguo = [];
 let money = 8888;
 
-let last_player_id = 0;
+let next_player_id = 0;
 let current_player_id = 0;
-let current_round = 1;
+let current_round = 0;
 let player_names = [];
 let player_inputs = {};
+let round_results = [];
 
 
 function save_settings() {
@@ -46,7 +47,7 @@ function save_settings() {
         let data_to_save = {
             'last_checked_version': last_checked_version,
         };
-        storage['data_ji_ququ'] = JSON.stringify(data_to_save);
+        storage['data_ji_ququ2'] = JSON.stringify(data_to_save);
         return true;
     }
 }
@@ -57,11 +58,11 @@ function load_settings() {
     }
     else {
         console.log('加载已保存的设置');
-        if (!('data_ji_ququ' in window.localStorage)) {
+        if (!('data_ji_ququ2' in window.localStorage)) {
             console.log('没有找到已保存的设置');
             return false;
         }
-        let data = JSON.parse(window.localStorage['data_ji_ququ']);
+        let data = JSON.parse(window.localStorage['data_ji_ququ2']);
         // 最后确认过的版本号
         if ('last_checked_version' in data) {
             last_checked_version = data['last_checked_version'];
@@ -192,27 +193,24 @@ function set_plan(plan) {
     for (let round = 0; round <= current_round; round++) {
         let p = player_inputs[current_player_id][round];
         plan_a.push(Math.abs(p));
-        plan_b.push(parseInt(p / Math.abs(p)));        
-        console.log(`round ${round + 1}`)
-        if (round < 5 && plan_a[round] == 100) {
-            continue
-        }
+        plan_b.push(parseInt((p / Math.abs(p)) * (round_results[round] || 1)));
+        console.log(`round ${round + 1}`);
         let input = Math.ceil(money * plan_a[round] / 100)
-        income = plan_b[round] > 0 ? Math.ceil(input * combos[current_combo] * plan_b[round] / 100) : -input;
-        console.log(plan_a, plan_b)
+        income = plan_b[round] > 0 ? Math.ceil(input * combos[current_combo] / 100) : -input;
+        console.log(plan_a, plan_b);
         console.log(
             `本金`, money,
-            `投注比例`, Math.abs(plan_a[round] * plan_b[round] / 100),
+            `投注比例`, Math.abs(plan_a[round] / 100),
             `投注`, input,
             `当前连胜`, current_combo,
             `赔率`, combos[current_combo] / 100,
-            `预计收益`, income,
-        )
-        document.getElementById(`td_temp_result`).innerHTML = `${player ? player + '<br>' : ''}
-            本金 ${money} 投注比例 ${Math.abs(plan_a[round] * plan_b[round] / 100)} 投注 ${input}<br>
-            当前连胜 ${current_combo} 赔率 ${combos[current_combo] / 100}<br>
-            预计收益 ${income}<br>
-        `;
+            `预计收益 ${Math.ceil(input * combos[current_combo] / 100)} / ${-input}`,
+        );
+        let player_name = player || `匿名玩家${current_player_id}`;
+        document.getElementById(`td_temp_result`).innerHTML = `<${player_name}>
+            本金 ${money}，投注比例 ${Math.abs(plan_a[round] / 100)}，投注 ${input}，
+            当前连胜 ${current_combo}，赔率 ${combos[current_combo] / 100}，
+            预计收益 ${Math.ceil(input * combos[current_combo] / 100)} / ${-input}`;
         if (money == 0 || rounds_alive < 0) {
             continue
         }
@@ -231,87 +229,127 @@ function set_plan(plan) {
             rounds_alive = -rounds_alive;
         }
     }
-    document.getElementById(`div_confirm`).style.display = '';
+    document.getElementById('div_inputs_1').style.display = 'none';
+    document.getElementById('div_inputs_2').style.display = 'none';
+    document.getElementById(`td_inputs_${current_player_id}_${current_round}`).innerHTML = `${plan > 0 ? '左' : '右'}${Math.abs(plan) == 100 ? 'ALL' : (Math.abs(plan) + '%')}`;
+    document.getElementById(`td_inputs_${current_player_id}_${current_round}`).style.backgroundColor = '';
 }
 
-function confirm_input() {
-    let money = 8888;
-    let plan_a = [], plan_b = [];
-    let current_combo = 0;
-    let rounds_alive = 0;
-    let income = 0;
-    let plan = player_inputs[current_player_id][current_round];
-    for (let round = 0; round <= current_round; round++) {
-        let p = player_inputs[current_player_id][round];
-        plan_a.push(Math.abs(p));
-        plan_b.push(parseInt(p / Math.abs(p)));        
-        if (money == 0 || rounds_alive < 0) {
-            income = 0;
-            continue
+function confirm_round(result) {
+    round_results.push(result);
+    for (let pid = 0; pid < next_player_id; pid++) {
+        let money = 8888;
+        let plan_a = [], plan_b = [];
+        let current_combo = 0;
+        let rounds_alive = 0;
+        let income = 0;
+        let plan = player_inputs[pid][current_round];
+        for (let round = 0; round <= current_round; round++) {
+            let p = player_inputs[pid][round];
+            if (p === undefined) {
+                continue;
+            }
+            plan_a.push(Math.abs(p));
+            plan_b.push(parseInt((p / Math.abs(p)) * round_results[round]));  
+            if (money == 0 || rounds_alive < 0) {
+                income = 0;
+                continue;
+            }
+            let input = Math.ceil(money * plan_a[round] / 100);
+            income = plan_b[round] > 0 ? Math.ceil(input * combos[current_combo] / 100) : -input;
+            money += income;
+            if (plan_b[round] > 0) {
+                current_combo += 1;
+            }
+            else {
+                current_combo = 0;
+            }
+            if (money > 0) {
+                rounds_alive += 1;
+            }
+            else {
+                rounds_alive = -rounds_alive;
+            }
         }
-        if (round < 5 && plan_a[round] == 100) {
-            continue
-        }
-        let input = Math.ceil(money * plan_a[round] / 100);
-        income = plan_b[round] > 0 ? Math.ceil(input * combos[current_combo] * plan_b[round] / 100) : -input;
-        money += income;
-        if (plan_b[round] > 0) {
-            current_combo += 1;
+        console.log('结束后本金', money)
+        document.getElementById('div_inputs_1').style.display = 'none';
+        document.getElementById('div_inputs_2').style.display = 'none';
+        if (plan === undefined) {
+            document.getElementById(`td_inputs_${pid}_${current_round}`).innerHTML = '未投注';
         }
         else {
-            current_combo = 0;
-        }
-        if (money > 0) {
-            rounds_alive += 1;
-        }
-        else {
-            rounds_alive = -rounds_alive;
+            document.getElementById(`td_inputs_${pid}_${current_round}`).innerHTML = `${money}<br>(${plan > 0 ? '左' : '右'}${Math.abs(plan) == 100 ? 'ALL' : (Math.abs(plan) + '%')} ${plan * result > 0 ? '赢' : '输'} ${plan * result > 0 ? '+' : income == 0 ? '-' : ''}${income})`;
+            document.getElementById(`td_inputs_${pid}_${current_round}`).style.backgroundColor = '';
+            document.getElementById(`td_money_${pid}`).innerHTML = money;
+            if (current_round < 9) {
+                document.getElementById(`div_inputs_${pid}_${current_round + 1}`).style.display = '';
+            }
         }
     }
-    console.log('结束后本金', money)
-    document.getElementById('div_inputs').style.display = 'none';
-    document.getElementById(`td_inputs_${current_player_id}_${current_round}`).innerHTML = `${money}<br>(${Math.abs(plan) == 100 ? 'ALL' : (Math.abs(plan) + '%')} ${plan > 0 ? '赢' : '输'} ${plan > 0 ? '+' : income == 0 ? '-' : ''}${income})`;
-    document.getElementById(`td_inputs_${current_player_id}_${current_round}`).style.backgroundColor = '';
-    document.getElementById(`td_money_${current_player_id}`).innerHTML = money;
-    if (current_round < 9) {
-        document.getElementById(`div_inputs_${current_player_id}_${current_round + 1}`).style.display = '';
+    document.getElementById(`th_round_${current_round}`).innerHTML += `<br>${result > 0 ? '左' : '右'}边赢`;
+    for (let pid = 0; pid < next_player_id; pid++) {
+        document.getElementById(`td_inputs_${pid}_${current_round}`).style.backgroundColor = '';
     }
+    current_round += 1;
     document.getElementById(`td_temp_result`).innerHTML = '';
-    document.getElementById(`div_confirm`).style.display = 'none';
+    if (current_round < 10) {
+        document.getElementById(`th_confirm`).innerHTML = `结算第${current_round + 1}轮`;
+        document.getElementById(`div_button_confirm_1`).innerHTML = `左边赢`;
+        document.getElementById(`div_button_confirm_2`).innerHTML = `右边赢`;
+    } 
+    else {
+        document.getElementById(`th_confirm`).innerHTML = `结课！`;
+        document.getElementById(`div_button_confirm_1`).style.display = 'none';
+        document.getElementById(`div_button_confirm_2`).style.display = 'none';
+    }
 }
 
 function add_player() {
-    let new_player = `<tr id="tr_player_${last_player_id}">
+    let new_player = `<tr id="tr_player_${next_player_id}">
         <td colspan="1">
-            <input type="text" id="input_player_id_${last_player_id}">
+            <input type="text" id="input_player_id_${next_player_id}" placeholder="匿名玩家${next_player_id}">
         </td>`;
     for (let i = 0; i < 10; i++) {
-        let display = i > 0 ? 'style="display: none;"' : '';
-        new_player += `<td colspan="1" id="td_inputs_${last_player_id}_${i}">
-            <div class="div_button" id="div_inputs_${last_player_id}_${i}" ${display} onclick="handle_inputs(${last_player_id}, ${i});">
-                第${i + 1}轮
+        new_player += `<td colspan="1" id="td_inputs_${next_player_id}_${i}">
+            <div class="div_button" id="div_inputs_${next_player_id}_${i}" style="display: none;" onclick="handle_inputs(${next_player_id}, ${i});">
+                投注
             </div>
         </td>`
     }
-    new_player += `<td colspan="1" id="td_money_${last_player_id}">8888</td></tr>`;
+    new_player += `<td colspan="1" id="td_money_${next_player_id}">8888</td></tr>`;
     player_names = [];
-    for (let i = 0; i < last_player_id; i++) {
+    for (let i = 0; i < next_player_id; i++) {
         player_names.push(document.getElementById(`input_player_id_${i}`).value);
     }
     document.getElementById('table_main').innerHTML += new_player;
-    player_inputs[last_player_id] = Array(10);
-    for (let i = 0; i < last_player_id; i++) {
+    player_inputs[next_player_id] = Array(10);
+    for (let i = 0; i < next_player_id; i++) {
         document.getElementById(`input_player_id_${i}`).value = player_names[i];
     }
-    last_player_id += 1;
+    next_player_id += 1;
+    document.getElementById('div_button_start').innerHTML = `所有 ${next_player_id} 名玩家都已准备好，赚笔大的！`;
+}
+
+function game_start() {
+    document.getElementById('tr_start').style.display = 'none';
+    document.getElementById('tr_player_add').style.display = 'none';
+    document.getElementById('tr_inputs_1').style.display = '';
+    document.getElementById('tr_inputs_2').style.display = '';
+    for (let pid = 0; pid < next_player_id; pid++) {
+        document.getElementById(`div_inputs_${pid}_0`).style.display = '';
+    }
 }
 
 function handle_inputs(player_id, round) {
-    document.getElementById(`td_inputs_${current_player_id}_${current_round}`).style.backgroundColor = '';
     current_player_id = player_id;
-    current_round = round;
-    document.getElementById('div_inputs').style.display = '';
-    document.getElementById('div_button_all_1').style.display = current_round < 5 ? 'none' : '';
-    document.getElementById('div_button_all_2').style.display = current_round < 5 ? 'none' : '';
+    // current_round = round;
+    document.getElementById(`td_inputs_${current_player_id}_${round}`).style.backgroundColor = '';
+    document.getElementById('div_inputs_1').style.display = '';
+    document.getElementById('div_inputs_2').style.display = '';
+    document.getElementById('div_button_all_1').style.display = round < 5 ? 'none' : '';
+    document.getElementById('div_button_all_2').style.display = round < 5 ? 'none' : '';
+    for (let pid = 0; pid < next_player_id; pid++) {
+        document.getElementById(`td_inputs_${pid}_${round}`).style.backgroundColor = '';
+    }
     document.getElementById(`td_inputs_${player_id}_${round}`).style.backgroundColor = '#fedcba';
 }
